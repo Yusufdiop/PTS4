3646e7f77620e9a45726f39c498389e5eb083011
Yusufdiop <106676594+Yusufdiop@users.noreply.github.com>
2022-06-15 18:34:08 +0000

    Add files via upload

---
+++ style.php
@@ -0,0 +1,11 @@
+<?php
+  header('content-type: text/css; charset:UTF-8');
+  $primary_color = "black";
+?>
+body {
+  height: 100px;
+  margin: 360px;
+  color: <?=$primary_color?>;
+  display: inline-block;
+
+}
